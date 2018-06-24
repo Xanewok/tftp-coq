0e033d87bd58f750bc73a9344e6d706314d2e562
Igor Matuszewski <Xanewok@gmail.com>
2018-06-24 17:04:47 +0200

    Add remaining packet datatype definitions

--- core/Packet.v
+++ core/Packet.v
@@ -14,10 +14,10 @@ Inductive opcode: Type :=
 
 Definition opcode_value (op : opcode) : positive :=
     match op with
-        | RRQ => 1
-        | WRQ => 2
-        | DATA => 3
-        | ACK => 4
+        | RRQ   => 1
+        | WRQ   => 2
+        | DATA  => 3
+        | ACK   => 4
         | ERROR => 5
     end.
 
@@ -29,6 +29,39 @@ Proof.
     destruct A.
     all: easy.
 Qed.
+
+Inductive mode: Type :=
+    | Netascii
+    | Octet.
+
+Inductive errcode: Type :=
+    | Undefined       (* Not defined, see error message (if any). *)
+    | FileNotFound    (* File not found. *)
+    | AccessViolation (* Access violation. *)
+    | AllocError      (* Disk full or allocation exceeded. *)
+    | IllegalOp       (* Illegal TFTP operation. *)
+    | UnknownTID      (* Unknown transfer ID. *)
+    | AlreadyExists   (* File already exists. *)
+    | NoSuchUser.     (* No such user. *)
+
+Definition errcode_value (err : errcode) : N :=
+    match err with
+        | Undefined       => 0
+        | FileNotFound    => 1
+        | AccessViolation => 2
+        | AllocError      => 3
+        | IllegalOp       => 4
+        | UnknownTID      => 5
+        | AlreadyExists   => 6
+        | NoSuchUser      => 7
+    end.
+
+Inductive packet : Set :=
+    | ReadReq        : string -> mode -> packet
+    | WriteReq       : string -> mode -> packet
+    | Data           : N -> string -> packet
+    | Acknowledgment : N -> packet
+    | Error          : errcode -> string -> packet.
 
 Local Open Scope char_scope.
 Definition parse_opcode (data : string) : option (opcode * string) :=
